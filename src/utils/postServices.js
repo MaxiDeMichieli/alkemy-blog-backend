da23dabea2a96bf6153659a838be851be90f3d5b
MaxiDeMichieli <maxidemichieli@gmail.com>
2020-12-21 12:29:05 -0300

    Add order and modify refactorPost in postServices

--- src/utils/postServices.js
+++ src/utils/postServices.js
@@ -1,14 +1,19 @@
 const db = require('../database/models');
 
 const include = [{ association: 'category' }];
+const order = [['created_at', 'DESC']];
 
 const postServices = {
   refactorPost: (post) => {
     const refactoredPost = {
       id: post.id,
       title: post.title,
+      content: post.content,
       image: post.image,
-      category: post.category.category,
+      category: {
+        id: post.category_id,
+        name: post.category.category,
+      },
       creationDate: post.createdAt,
     };
     return refactoredPost;
@@ -22,7 +27,7 @@ const postServices = {
     };
     return data;
   },
-  postsFindAll: (where) => db.Posts.findAll({ where, include }),
+  postsFindAll: (where) => db.Posts.findAll({ where, order, include }),
   postsFindOne: (where) => db.Posts.findOne({ where, include }),
   postsCreate: (body) => db.Posts.create(postServices.postToSave(body)),
   postsUpdate: (body, where) => db.Posts.update(postServices.postToSave(body), { where }),
